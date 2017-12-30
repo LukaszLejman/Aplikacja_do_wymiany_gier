88ac4ed816ea673d750b009e1d00df84d760a6dd
LukaszLejman <LukaszLejman96@wp.pl>
2017-12-30 15:53:07 +0100

    start

---
+++ src/app/games/games.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GamesListComponent } from './games-list/games-list.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GamesListComponent, GameDetailsComponent]
+})
+export class GamesModule { }
