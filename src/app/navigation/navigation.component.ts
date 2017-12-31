fedd9911abc9bc862420284ea519716191399e1c
LukaszLejman <LukaszLejman96@wp.pl>
2017-12-31 22:03:41 +0100

    log off

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) { }
   public loginStatus: boolean = false;
   ngOnInit() {
     if (JSON.parse(sessionStorage.getItem('logged')) !== null) {
       this.loginStatus = JSON.parse(sessionStorage.getItem('logged'));
     }
+  }
+  logOff() {
+    sessionStorage.clear();
+    this.router.navigate(['home']);
+    window.location.reload();
   }
 }
